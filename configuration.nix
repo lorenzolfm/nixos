3696f61ba835e652dae2ae45bf476c7187bb6a16
Lorenzo <maturanolorenzo@gmail.com>
2025-02-16 16:43:23 -0300

    feat: open 9000 port

--- configuration.nix
+++ configuration.nix
@@ -214,7 +214,7 @@
   # services.openssh.enable = true;
 
   # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
+networking.firewall.allowedTCPPorts = [ 9000 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
   # networking.firewall.enable = false;
