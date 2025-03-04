b3d91e6f612af7e032b50c5caa5d9f6f2c1edd30
Lorenzo Maturano <maturanolorenzo@gmail.com>
2025-03-04 16:48:58 -0300

    Flake.nix (#1)
    * feat(flake): trying to use flake.nix
    
    * feat(desktop): removing comments
    
    * feat(desktop): installing firacode
    
    * feat(flake): using flake.nix to unify all configs

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "My NixOS enjoyrr config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    darwin.url = "github:LnL7/nix-darwin/master";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, darwin, ... }@inputs: {
+    nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./hosts/desktop/configuration.nix
+      ];
+    };
+  };
+}
